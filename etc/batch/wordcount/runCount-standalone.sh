b1476bf6b9edf68337c80cf25284227277d238ec
Chris Ruegger <chris.ruegger@gmail.com>
2018-06-25 19:33:34 -0400

    run scripts

--- etc/batch/wordcount/runCount-standalone.sh
+++ etc/batch/wordcount/runCount-standalone.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 MASTER=spark://captain:7077
-JAR_FILE=/home/hadoop/code/spark/MySpark/build/libs/MySpark.jar
+JAR_FILE=/home/hadoop/code/Spark1/build/libs/Spark1-all-1.0.jar
+
+INPUT_PATH=hdfs://captain:9000/inputs/hamlet.txt
+OUTPUT_PATH=hdfs://captain:9000/outputs/WordCount/
 
 # Remove output
+rm -f out
 hdfs dfs -rm -r /outputs/WordCount
 
 # Submit
-spark-submit --class com.rueggerllc.spark.apps.WordCount --master $MASTER $JAR_FILE
+spark-submit --class com.rueggerllc.spark.batch.WordCount --master $MASTER $JAR_FILE $INPUT_PATH $OUTPUT_PATH
+
+# Output Results
+hdfs dfs -cat /outputs/WordCount/part* > out
+
--- etc/batch/wordcount/runCount-yarn.sh
+++ etc/batch/wordcount/runCount-yarn.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 MASTER=yarn
-DEPLOY_MODE=cluster
-JAR_FILE=/home/hadoop/code/spark/MySpark/build/libs/MySpark.jar
+JAR_FILE=/home/hadoop/code/Spark1/build/libs/Spark1-all-1.0.jar
 
-# Cleanup
+INPUT_PATH=hdfs://captain:9000/inputs/hamlet.txt
+OUTPUT_PATH=hdfs://captain:9000/outputs/WordCount/
+
+# Remove output
+rm -f out
 hdfs dfs -rm -r /outputs/WordCount
 
 # Submit
-spark-submit --class com.rueggerllc.spark.apps.WordCount --master $MASTER --deploy-mode $DEPLOY_MODE $JAR_FILE
+spark-submit --class com.rueggerllc.spark.batch.WordCount --master $MASTER $JAR_FILE $INPUT_PATH $OUTPUT_PATH
+
+# Output Results
+hdfs dfs -cat /outputs/WordCount/part* > out
+
